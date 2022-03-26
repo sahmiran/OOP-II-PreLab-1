e4d1938cfb1ddc201136ccf5673a27c30f449736
sahmiran <hasancantural19@gmail.com>
2022-03-27 02:08:53 +0300

    Login ekrani icin iki textbox, iki Label ve bir button  eklendi. admin ve username kullanicilari icin login islemi gerceklestirildi

---
+++ OOP-II-Lab/OOP-II-Lab/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace OOP_II_Lab
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loginBtn = new System.Windows.Forms.Button();
+            this.usernameText = new System.Windows.Forms.TextBox();
+            this.passwordText = new System.Windows.Forms.TextBox();
+            this.usernameLabel = new System.Windows.Forms.Label();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // loginBtn
+            // 
+            this.loginBtn.Location = new System.Drawing.Point(91, 296);
+            this.loginBtn.Name = "loginBtn";
+            this.loginBtn.Size = new System.Drawing.Size(213, 40);
+            this.loginBtn.TabIndex = 0;
+            this.loginBtn.Text = "Giriş ";
+            this.loginBtn.UseVisualStyleBackColor = true;
+            this.loginBtn.Click += new System.EventHandler(this.loginBtn_Click);
+            // 
+            // usernameText
+            // 
+            this.usernameText.Location = new System.Drawing.Point(91, 184);
+            this.usernameText.Multiline = true;
+            this.usernameText.Name = "usernameText";
+            this.usernameText.Size = new System.Drawing.Size(213, 23);
+            this.usernameText.TabIndex = 1;
+            // 
+            // passwordText
+            // 
+            this.passwordText.Location = new System.Drawing.Point(91, 237);
+            this.passwordText.Name = "passwordText";
+            this.passwordText.PasswordChar = '*';
+            this.passwordText.Size = new System.Drawing.Size(213, 23);
+            this.passwordText.TabIndex = 2;
+            // 
+            // usernameLabel
+            // 
+            this.usernameLabel.AutoSize = true;
+            this.usernameLabel.Location = new System.Drawing.Point(12, 187);
+            this.usernameLabel.Name = "usernameLabel";
+            this.usernameLabel.Size = new System.Drawing.Size(73, 15);
+            this.usernameLabel.TabIndex = 3;
+            this.usernameLabel.Text = "Kullanici Adi";
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Location = new System.Drawing.Point(12, 245);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(30, 15);
+            this.passwordLabel.TabIndex = 4;
+            this.passwordLabel.Text = "Şifre";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(319, 359);
+            this.Controls.Add(this.passwordLabel);
+            this.Controls.Add(this.usernameLabel);
+            this.Controls.Add(this.passwordText);
+            this.Controls.Add(this.usernameText);
+            this.Controls.Add(this.loginBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button loginBtn;
+        private TextBox usernameText;
+        private TextBox passwordText;
+        private Label usernameLabel;
+        private Label passwordLabel;
+    }
+}
---
+++ OOP-II-Lab/OOP-II-Lab/Form1.cs
@@ -0,0 +1,30 @@
+namespace OOP_II_Lab
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void loginBtn_Click(object sender, EventArgs e)
+        {
+            if (usernameText.Text == "admin" && passwordText.Text == "admin")
+            {
+                new Form2().Show();
+                this.Hide();
+            }
+
+            else if (usernameText.Text == "user" && passwordText.Text == "user")
+            {
+                new Form2().Show();
+                this.Hide();
+            }
+
+            else
+            {
+                MessageBox.Show("Kullanici Adi yada Sifre hatali");
+            }
+        }
+    }
+}
