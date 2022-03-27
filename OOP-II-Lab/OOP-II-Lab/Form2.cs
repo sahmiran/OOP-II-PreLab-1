24c19e4e03dccc4e6230f24eaaf56f4af4934e4b
sahmiran <hasancantural19@gmail.com>
2022-03-27 13:18:13 +0300

    Kullanici ayarlari cikilsa bile  uygulamada kayitli kaliyor

--- OOP-II-Lab/OOP-II-Lab/Form2.cs
+++ OOP-II-Lab/OOP-II-Lab/Form2.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using OOP_II_Lab.Properties;
 
 namespace OOP_II_Lab
 {
@@ -15,6 +16,8 @@ namespace OOP_II_Lab
         public Form2()
         {
             InitializeComponent();
+            selectedDifficultyLabel.Text = Settings.Default["Difficulty"].ToString();
+            selectedShapeLabel.Text = Settings.Default["Shape"].ToString();
         }
 
         private void hideSubMenu()
@@ -49,17 +52,26 @@ namespace OOP_II_Lab
 
         private void easyBtn_Click(object sender, EventArgs e)
         {
+            Settings.Default["Difficulty"] = easyBtn.Text;
+            Settings.Default.Save();
+            selectedDifficultyLabel.Text = easyBtn.Text;
             hideSubMenu();
         }
 
         private void normalBtn_Click(object sender, EventArgs e)
         {
+            Settings.Default["Difficulty"] = normalBtn.Text;
+            Settings.Default.Save();
+            selectedDifficultyLabel.Text = normalBtn.Text;
             hideSubMenu();
         }
 
         
         private void hardBtn_Click(object sender, EventArgs e)
         {
+            Settings.Default["Difficulty"] = hardBtn.Text;
+            Settings.Default.Save();
+            selectedDifficultyLabel.Text = hardBtn.Text;
             hideSubMenu();
         }
 
@@ -80,7 +92,46 @@ namespace OOP_II_Lab
 
         private void customSaveBtn_Click(object sender, EventArgs e)
         {
+            string tempShape = "";
+            tempShape = xValueText.Text.ToString() + " x " + yValueText.Text.ToString();
+            Settings.Default["Difficulty"] = tempShape;
+            Settings.Default.Save();
+            selectedDifficultyLabel.Text = tempShape;
             hideSubMenu();
+        }
+
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            new Form1().Show();
+            this.Hide();
+        }
+
+        private void saveoptionBtn_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void shapeSaveBtn_Click(object sender, EventArgs e)
+        {
+            string temp = "";
+            if (squareCheckBox.Checked == true)
+            {
+                temp = temp + squareCheckBox.Text.ToString() + " ";
+            }
+
+            if (triangleCheckBox.Checked == true)
+            {
+                temp = temp + triangleCheckBox.Text.ToString() + " ";
+            }
+
+            if (roundCheckBox.Checked == true)
+            {
+                temp = temp + roundCheckBox.Text.ToString() + " ";
+            }
+
+            Settings.Default["Shape"] = temp;
+            Settings.Default.Save();
+            selectedShapeLabel.Text = temp;
         }
     }
 }
